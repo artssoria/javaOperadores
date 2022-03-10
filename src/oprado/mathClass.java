4a92e0c274119a1e15e7beb3b4870f5c5262ee0c
artssoria <arts.soria9@gmail.com>
2022-03-10 01:46:32 -0300

    clase math

---
+++ src/oprado/mathClass.java
@@ -0,0 +1,18 @@
+/*
+Clase Math
+ */
+package oprado;
+
+/**
+ *
+ * @author arturo
+ */
+public class mathClass {
+    public static void main(String[] args) {
+        double  raiz = Math.sqrt(9);
+        
+        System.out.println(raiz);
+        
+    }
+    
+}
