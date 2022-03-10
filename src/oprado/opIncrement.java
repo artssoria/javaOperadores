118a67e3c7d3b2def000b5acbc205ded719e4984
artssoria <arts.soria9@gmail.com>
2022-03-10 01:15:07 -0300

    commit de incremento

---
+++ src/oprado/opIncrement.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oprado;
+
+/**
+ *
+ * @author arturo
+ */
+public class opIncrement {
+    public static void main(String[] args) {
+        int  x = 5;
+        x++;
+        System.out.println(x);
+    }
+    
+}
