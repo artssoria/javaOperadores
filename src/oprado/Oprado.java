968e93f64a6d71456ded6ab9b83c149cbfbee1ab
artssoria <arts.soria9@gmail.com>
2022-03-10 00:55:57 -0300

    Commit de operadores

---
+++ src/oprado/Oprado.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oprado;
+
+/**
+ *
+ * @author arturo
+ */
+public class Oprado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = 10;
+        numero  = numero + 5;
+        System.out.println("el numero es " + numero);
+    }
+    
+}
