7c7cc15af78e2083995330322fc9e82e4deec463
artssoria <arts.soria9@gmail.com>
2022-03-10 02:52:38 -0300

    Ejercicio 2

---
+++ src/oprado/ejercicio2.java
@@ -0,0 +1,16 @@
+/*
+hacer un program que calc e impr el salario semanal
+de un empleado a partir de sus horas semanales 
+trabajas y de su salario por hora
+ */
+package oprado;
+
+/**
+ *
+ * @author arturo
+ */
+public class ejercicio2 {
+    public static void main(String[] args) {
+        
+    }
+}
