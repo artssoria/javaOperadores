268cbabab9cfb3b045d3cce3bd9f799d6b0b3461
artssoria <arts.soria9@gmail.com>
2022-03-10 23:44:45 -0300

    Ejercicio 2 finalizado

--- src/oprado/ejercicio2.java
+++ src/oprado/ejercicio2.java
@@ -5,12 +5,25 @@ trabajas y de su salario por hora
  */
 package oprado;
 
+import java.util.Scanner;
+
 /**
  *
  * @author arturo
  */
 public class ejercicio2 {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int horaSemanales;
+        float salarioHoras, salarioTotal;
         
+        System.out.println("Digite las horas semanales trabajadas: ");
+        horaSemanales = entrada.nextInt();
+        System.out.println("Digite el salario que gana por horas:  ");
+        salarioHoras = entrada.nextFloat();
+        
+        salarioTotal = horaSemanales * salarioHoras;
+        
+        System.out.println("\nEl salario semanal es: " + salarioTotal);
     }
 }
