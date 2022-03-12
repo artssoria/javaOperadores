88c7b98bbac29d4f5a78787bb46941b6bc805dbe
artssoria <arts.soria9@gmail.com>
2022-03-12 17:17:17 -0300

    ejercicio 1 de operadores

---
+++ src/oprado/ejercicioCondicionales1.java
@@ -0,0 +1,25 @@
+/*
+hacer un program que lea un numero entero
+y muestre si el numero es multiplo de 10
+ */
+package oprado;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author arturo
+ */
+public class ejercicioCondicionales1 {
+    public static void main(String[] args) {
+        int numero;
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("digite un numero multiplo de 10 "));
+        if (numero%10==0) {
+            JOptionPane.showMessageDialog(null, "el numero " + numero+ " es multiplo de 10");
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "el numero no es multiplo de 10");
+        }
+    }
+}
